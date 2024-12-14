2eaaca2c056770f0fa8da3cbd35f9f64b7994541
MirheydarA <a.mirheyder004@gmail.com>
2024-12-15 00:55:47 +0400

    SomeChanges

--- src/pages/documentspage/index.jsx
+++ src/pages/documentspage/index.jsx
@@ -11,6 +11,7 @@ import DeleteIcon from '@mui/icons-material/Delete';
 
 export const Documents = () => {
   const [documents, setDocuments] = useState([]);
+  const [instrumentDocuments, setInstrumentDocuments] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
@@ -23,7 +24,7 @@ export const Documents = () => {
     setError(null);
     try {
       const response = await projectDocumentsService.getAllDeleted();
-      setDocuments((prevDocuments) => [...prevDocuments, ...response.data]);
+      setDocuments(response.data);
     } catch (err) {
       console.log(err);
     } finally {
@@ -40,7 +41,7 @@ export const Documents = () => {
     setError(null);
     try {
       const response = await instrumentDocumentsService.getAllDeleted();
-      setDocuments((prevDocuments) => [...prevDocuments, ...response.data]);
+      setInstrumentDocuments(response.data);
     } catch (err) {
       console.log(err);
     } finally {
@@ -58,6 +59,17 @@ export const Documents = () => {
       alert("An error occurred while deleting the document.");
     }
   };
+
+  const handleDeleteInstrumentDocument = async (documentId) => {
+    try {
+      await instrumentDocumentsService.hardDelete(documentId);
+      setInstrumentDocuments((prevDocuments) => prevDocuments.filter((doc) => doc.id !== documentId));
+    } catch (err) {
+      console.error("Failed to delete document:", err);
+      alert("An error occurred while deleting the document.");
+    }
+  };
+
 
   const formatFileName = (fileName) => {
     const parts = fileName?.split(' ');
@@ -78,32 +90,61 @@ export const Documents = () => {
         <Typography color="error">{error}</Typography>
       ) : (
         <Grid container spacing={2}>
-          {documents.length > 0 ? (
-            documents.map((doc) => (
-              <Grid item xs={12} sm={6} key={doc.id}>
-                <Box display="flex" alignItems="center" sx={{ padding: 1, border: '1px solid #e0e0e0', borderRadius: 2 }}>
-                  {doc.fileType === 'pdf' ? (
-                    <PictureAsPdfIcon sx={{ fontSize: 40, color: '#D32F2F' }} />
-                  ) : (
-                    <DescriptionIcon sx={{ fontSize: 40 }} />
-                  )}
-                  <Typography
-                    variant="body1"
-                    sx={{ marginLeft: '10px', cursor: 'pointer', color: '#1D34D8', fontWeight: 'bold' }}
-                    onClick={() => window.open(`${process.env.REACT_APP_DOCUMENT_URL}/assets/projectpdf/${doc.fileName}`, '_blank')}
-                  >
-                  {formatFileName(doc.fileName)}
-                  </Typography>
-                  <IconButton onClick={() => handleDelete(doc.id)} sx={{ marginLeft: 'auto' }}>
-                    <DeleteIcon className='text-[#d33]' />
-                  </IconButton>
-                </Box>
-              </Grid>
-            ))
+          {documents.length > 0 || instrumentDocuments.length > 0 ? (
+            <>
+              {documents.map((doc) => (
+                <Grid item xs={12} sm={6} key={doc.id}>
+                  <Box display="flex" alignItems="center" sx={{ padding: 1, border: '1px solid #e0e0e0', borderRadius: 2 }}>
+                    {doc.fileType === 'pdf' ? (
+                      <PictureAsPdfIcon sx={{ fontSize: 40, color: '#D32F2F' }} />
+                    ) : (
+                      <DescriptionIcon sx={{ fontSize: 40 }} />
+                    )}
+                    <Typography
+                      variant="body1"
+                      sx={{ marginLeft: '10px', cursor: 'pointer', color: '#1D34D8', fontWeight: 'bold' }}
+                      onClick={() =>
+                        window.open(`${process.env.REACT_APP_DOCUMENT_URL}/assets/projectpdf/${doc.fileName}`, '_blank')
+                      }
+                    >
+                      {formatFileName(doc.fileName)}
+                    </Typography>
+                    <IconButton onClick={() => handleDelete(doc.id)} sx={{ marginLeft: 'auto' }}>
+                      <DeleteIcon className="text-[#d33]" />
+                    </IconButton>
+                  </Box>
+                </Grid>
+              ))}
+              {instrumentDocuments.map((doc) => (
+                <Grid item xs={12} sm={6} key={doc.id}>
+                  <Box display="flex" alignItems="center" sx={{ padding: 1, border: '1px solid #e0e0e0', borderRadius: 2 }}>
+                    {doc.fileType === 'pdf' ? (
+                      <PictureAsPdfIcon sx={{ fontSize: 40, color: '#D32F2F' }} />
+                    ) : (
+                      <DescriptionIcon sx={{ fontSize: 40 }} />
+                    )}
+                    <Typography
+                      variant="body1"
+                      sx={{ marginLeft: '10px', cursor: 'pointer', color: '#1D34D8', fontWeight: 'bold' }}
+                      onClick={() =>
+                        window.open(`${process.env.REACT_APP_DOCUMENT_URL}/assets/instrumentpdf/${doc.fileName}`, '_blank')
+                      }
+                    >
+                      {formatFileName(doc.fileName)}
+                    </Typography>
+                    <IconButton onClick={() => handleDeleteInstrumentDocument(doc.id)} sx={{ marginLeft: 'auto' }}>
+                      <DeleteIcon className="text-[#d33]" />
+                    </IconButton>
+                  </Box>
+                </Grid>
+              ))}
+            </>
           ) : (
-            <Typography variant="body2" color="textSecondary">
+            <Typography variant="body2" color="textSecondary" className='!ml-4'>
+              No documents in trash bin.
             </Typography>
           )}
+
         </Grid>
       )}
     </Box>
